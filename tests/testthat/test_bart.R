79cdeb2e5a678b4562139d04a399671e59b35b4e
Liang Zhang <psychelzh@outlook.com>
2020-05-13 11:44:51 +0800

    Enhance codes for testing
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test_bart.R
+++ tests/testthat/test_bart.R
@@ -1,22 +1,19 @@
 context("`bart` test")
 library(dataprocr2)
 test_that("`bart` should deal with normal and abnormal data", {
-  expect_equal(
-    bart(
+  expect_warning(
+    result_normal <- bart(
       jsonlite::read_json("data_bart.json", simplifyVector = TRUE)
-    )$mean_hits, 5
+    ), NA
   )
-  expect_equal(
-    bart(
-      jsonlite::read_json("data_bart.json", simplifyVector = TRUE)
-    )$mean_hits_raw, 5.2333, tolerance = 1e-4
+  expect_equal(result_normal$mean_hits, 5)
+  expect_equal(result_normal$mean_hits_raw, 5.2333, tolerance = 1e-4)
+  expect_equal(result_normal$is_normal, TRUE)
+  expect_warning(
+    result_abnormal <- bart(data.frame()),
+    "`NHit` and `Feedback` variables are required."
   )
-  expect_equal(
-    bart(
-      jsonlite::read_json("data_bart.json", simplifyVector = TRUE)
-    )$is_normal, TRUE
-  )
-  expect_equal(is.na(bart(data.frame())$mean_hits), TRUE)
-  expect_equal(is.na(bart(data.frame())$mean_hits_raw), TRUE)
-  expect_equal(bart(data.frame())$is_normal, FALSE)
+  expect_equal(is.na(result_abnormal$mean_hits), TRUE)
+  expect_equal(is.na(result_abnormal$mean_hits_raw), TRUE)
+  expect_equal(result_abnormal$is_normal, FALSE)
 })
--- tests/testthat/test_crt.R
+++ tests/testthat/test_crt.R
@@ -1,22 +1,19 @@
 context("`crt` test")
 library(dataprocr2)
 test_that("`crt` should deal with normal and abnormal data", {
-  expect_equal(
-    crt(
+  expect_warning(
+    result_normal <- crt(
       jsonlite::read_json("data_crt.json", simplifyVector = TRUE)
-    )$mrt, 326.494, tolerance = 1e-3
+    ), NA
   )
-  expect_equal(
-    crt(
-      jsonlite::read_json("data_crt.json", simplifyVector = TRUE)
-    )$count_correct, 334
+  expect_equal(result_normal$mrt, 326.494, tolerance = 1e-3)
+  expect_equal(result_normal$count_correct, 334)
+  expect_equal(result_normal$is_normal, TRUE)
+  expect_warning(
+    result_abnormal <- crt(data.frame()),
+    "`ACC` and `RT` variables are required."
   )
-  expect_equal(
-    crt(
-      jsonlite::read_json("data_crt.json", simplifyVector = TRUE)
-    )$is_normal, TRUE
-  )
-  expect_equal(is.na(crt(data.frame())$mrt), TRUE)
-  expect_equal(is.na(crt(data.frame())$count_correct), TRUE)
-  expect_equal(crt(data.frame())$is_normal, FALSE)
+  expect_equal(is.na(result_abnormal$mrt), TRUE)
+  expect_equal(is.na(result_abnormal$count_correct), TRUE)
+  expect_equal(result_abnormal$is_normal, FALSE)
 })
