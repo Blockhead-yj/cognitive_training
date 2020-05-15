2dccff13c8c4b912b673657810e184481d301ac7
Liang Zhang <psychelzh@outlook.com>
2020-05-16 01:01:48 +0800

    Add for Symbolic Number Comparison game
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ R/symncmp.R
@@ -0,0 +1,54 @@
+#' Calculates index scores for Symbolic Number Comparison game.
+#'
+#' Several values including percentage of correct responses (pc), mean reaction
+#' time (mrt), distance effect (dist_effect) and adjusted distance effect
+#' (dist_effect_adj).
+#'
+#' @param data Raw data of class \code{data.frame}.
+#' @param ... Other input argument for future expansion.
+#' @return A \code{data.frame} contains following values:
+#' \describe{
+#'   \item{pc}{Percentage of correct responses.}
+#'   \item{mrt}{Mean reaction time.}
+#'   \item{w}{Weber fraction.}
+#'   \item{is_normal}{Checking result whether the data is normal.}
+#' }
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @export
+symncmp <- function(data, ...) {
+  if (!all(utils::hasName(data, c("Small", "Big", "RT", "ACC")))) {
+    warning("`Small`, `Big`, `RT` and `ACC` variables are required.")
+    return(
+      data.frame(
+        pc = NA_real_,
+        mrt = NA_real_,
+        dist_effect = NA_real_,
+        dist_effect_adj = NA_real_,
+        is_normal = FALSE
+      )
+    )
+  }
+  # set as wrong for trials responding too quickly
+  data_adj <- data %>%
+    dplyr::mutate(acc_adj = dplyr::if_else(.data$RT <= 100, 0L, .data$ACC))
+  basic <- data_adj %>%
+    dplyr::summarise(
+      pc = mean(.data$acc_adj == 1),
+      mrt = mean(.data$RT[.data$acc_adj == 1])
+    )
+  data_dist_effect <- data_adj %>%
+    dplyr::filter(.data$acc_adj == 1) %>%
+    dplyr::mutate(dist = .data$Big - .data$Small)
+  dist_effect_orig <- stats::lm(RT ~ dist, data_dist_effect) %>%
+    stats::coef() %>%
+    `[`("dist")
+  dist_effect <- data.frame(
+    dist_effect = dist_effect_orig,
+    dist_effect_adj = dist_effect_orig / basic$mrt
+  )
+  is_normal <- data_adj %>%
+    dplyr::summarise(n = dplyr::n(), count_correct = sum(.data$acc_adj == 1)) %>%
+    dplyr::transmute(is_normal = .data$n > stats::qbinom(0.95, .data$n, 0.5))
+  cbind(basic, dist_effect, is_normal)
+}
---
+++ tests/testthat/test-symncmp.R
@@ -0,0 +1,27 @@
+context("`symncmp` test")
+library(dataprocr2)
+test_that("`symncmp` should deal with normal and abnormal data", {
+  # testing on normal data
+  expect_silent(
+    result_symncmp <- symncmp(
+      jsonlite::read_json("data_symncmp.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_symncmp, c("pc", "mrt", "dist_effect", "dist_effect_adj", "is_normal"))
+  expect_equal(result_symncmp$pc, 0.93, tolerance = 0.01)
+  expect_equal(result_symncmp$mrt, 568.373, tolerance = 0.001)
+  expect_equal(result_symncmp$dist_effect, 5.80, tolerance = 0.01)
+  expect_equal(result_symncmp$dist_effect_adj, 0.0102, tolerance = 1e-4)
+  expect_true(result_symncmp$is_normal)
+  # testing on abnormal data input
+  expect_warning(
+    result_abnormal <- symncmp(data.frame()),
+    "`Small`, `Big`, `RT` and `ACC` variables are required."
+  )
+  expect_named(result_abnormal, c("pc", "mrt", "dist_effect", "dist_effect_adj", "is_normal"))
+  expect_true(is.na(result_abnormal$pc))
+  expect_true(is.na(result_abnormal$mrt))
+  expect_true(is.na(result_abnormal$dist_effect))
+  expect_true(is.na(result_abnormal$dist_effect_adj))
+  expect_false(result_abnormal$is_normal)
+})
