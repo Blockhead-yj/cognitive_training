4c888a7b231b4965ed8246acb52b5c9dfd165149
Liang Zhang <psychelzh@outlook.com>
2020-05-16 16:10:19 +0800

    Add all the Cancellation games to `count` test
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test_count.R
+++ tests/testthat/test_count.R
@@ -64,6 +64,60 @@ test_that("`count` should deal with normal and abnormal data", {
   expect_named(result_calcmed, c("count_correct", "is_normal"))
   expect_equal(result_calcmed$count_correct, 115)
   expect_true(result_calcmed$is_normal)
+  # testing on Lexic game
+  expect_silent(
+    result_lexic <- count(
+      jsonlite::read_json("data_count/data_lexic.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_lexic, c("count_correct", "is_normal"))
+  expect_equal(result_lexic$count_correct, 86)
+  expect_true(result_lexic$is_normal)
+  # testing on Orthography game
+  expect_silent(
+    result_ortho <- count(
+      jsonlite::read_json("data_count/data_ortho.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_ortho, c("count_correct", "is_normal"))
+  expect_equal(result_ortho$count_correct, 88)
+  expect_true(result_ortho$is_normal)
+  # testing on Pinyin game
+  expect_silent(
+    result_pinyin <- count(
+      jsonlite::read_json("data_count/data_pinyin.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_pinyin, c("count_correct", "is_normal"))
+  expect_equal(result_pinyin$count_correct, 76)
+  expect_true(result_pinyin$is_normal)
+  # testing on Semanteme game
+  expect_silent(
+    result_seman <- count(
+      jsonlite::read_json("data_count/data_seman.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_seman, c("count_correct", "is_normal"))
+  expect_equal(result_seman$count_correct, 73)
+  expect_true(result_seman$is_normal)
+  # testing on Symbol game
+  expect_silent(
+    result_symbol <- count(
+      jsonlite::read_json("data_count/data_symbol.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_symbol, c("count_correct", "is_normal"))
+  expect_equal(result_symbol$count_correct, 141)
+  expect_true(result_symbol$is_normal)
+  # testing on Tone game
+  expect_silent(
+    result_tone <- count(
+      jsonlite::read_json("data_count/data_tone.json", simplifyVector = TRUE)
+    )
+  )
+  expect_named(result_tone, c("count_correct", "is_normal"))
+  expect_equal(result_tone$count_correct, 56)
+  expect_true(result_tone$is_normal)
   # testing on abnormal data input
   expect_warning(
     result_abnormal <- count(data.frame()),
