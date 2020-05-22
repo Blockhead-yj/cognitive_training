e5259c2a7f709e26829e170c05bb8c414420c344
Liang Zhang <psychelzh@outlook.com>
2020-05-23 00:13:45 +0800

    Add more indices for DRM games
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/drm.R
+++ R/drm.R
@@ -8,6 +8,9 @@
 #' @param ... Other input argument for future expansion.
 #' @return A \code{data.frame} contains following values:
 #' \describe{
+#'   \item{pc}{Percent of correct responses.}
+#'   \item{p_old_lure}{Percent of old response for "lure" stimuli.}
+#'   \item{p_old_foil}{Percent of old response for "foil" stimuli.}
 #'   \item{fm_ratio}{False memory ratio.}
 #'   \item{fm_dprime}{False memory d'.}
 #'   \item{is_normal}{Checking result whether the data is normal.}
@@ -20,17 +23,24 @@ drm <- function(data, ...) {
     warning("`Type`, `RT` and `ACC` variables are required.")
     return(
       data.frame(
+        pc = NA_real_,
+        p_old_lure = NA_real_,
+        p_old_foil = NA_real_,
         fm_ratio = NA_real_,
         fm_dprime = NA_real_,
         is_normal = FALSE
       )
     )
   }
+  pc_all <- data %>%
+    dplyr::summarise(pc = mean(.data$ACC == 1))
   fm <- data %>%
     dplyr::group_by(.data$Type) %>%
     dplyr::summarise(p_old = sum(.data$Resp == "Old") / dplyr::n()) %>%
     tidyr::pivot_wider(names_from = "Type", values_from = "p_old") %>%
     dplyr::transmute(
+      p_old_lure = .data$Lure,
+      p_old_foil = .data$Foil,
       fm_ratio = .data$Lure - .data$Foil,
       fm_dprime = stats::qnorm(.data$Lure) - stats::qnorm(.data$Foil)
     )
@@ -38,5 +48,5 @@ drm <- function(data, ...) {
     dplyr::mutate(acc_adj = dplyr::if_else(.data$RT >= 100, .data$ACC, 0L)) %>%
     dplyr::summarise(n = dplyr::n(), count_correct = sum(.data$acc_adj == 1)) %>%
     dplyr::transmute(is_normal = .data$n > stats::qbinom(0.95, .data$n, 0.5))
-  cbind(fm, is_normal)
+  cbind(pc_all, fm, is_normal)
 }
--- tests/testthat/test-drm.R
+++ tests/testthat/test-drm.R
@@ -1,20 +1,29 @@
 context("`drm` test")
 library(dataprocr2)
 test_that("`drm` should deal with normal and abnormal data", {
+  result_names <- c("pc", "p_old_lure", "p_old_foil", "fm_ratio", "fm_dprime", "is_normal")
+  # testing on normal data
   expect_silent(
     result_normal <- drm(
       jsonlite::read_json("data_drm.json", simplifyVector = TRUE)
     )
   )
-  expect_named(result_normal, c("fm_ratio", "fm_dprime", "is_normal"))
+  expect_named(result_normal, result_names)
+  expect_equal(result_normal$pc, 0.828, tolerance = 1e-3)
+  expect_equal(result_normal$p_old_lure, 0.28125, tolerance = 1e-5)
+  expect_equal(result_normal$p_old_foil, 0.0625, tolerance = 1e-4)
   expect_equal(result_normal$fm_ratio, 0.21875, tolerance = 1e-5)
   expect_equal(result_normal$fm_dprime, 0.9550, tolerance = 1e-4)
   expect_true(result_normal$is_normal)
+  # testing on abnormal data
   expect_warning(
     result_abnormal <- drm(data.frame()),
     "`Type`, `RT` and `ACC` variables are required."
   )
-  expect_named(result_abnormal, c("fm_ratio", "fm_dprime", "is_normal"))
+  expect_named(result_abnormal, result_names)
+  expect_true(is.na(result_abnormal$pc))
+  expect_true(is.na(result_abnormal$p_old_lure))
+  expect_true(is.na(result_abnormal$p_old_foil))
   expect_true(is.na(result_abnormal$fm_ratio))
   expect_true(is.na(result_abnormal$fm_dprime))
   expect_false(result_abnormal$is_normal)
