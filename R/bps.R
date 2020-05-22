8191fbf640271288d45ddf67a01db9aca34f45c9
Liang Zhang <psychelzh@outlook.com>
2020-05-22 23:41:50 +0800

    Add more indices for BPS game
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/bps.R
+++ R/bps.R
@@ -6,6 +6,10 @@
 #' @param ... Other input argument for future expansion.
 #' @return A \code{data.frame} contains following values:
 #' \describe{
+#'   \item{pc}{Percent of correct responses.}
+#'   \item{p_sim_lure}{Percent of similar responses for "lure" stimuli.}
+#'   \item{p_sim_foil}{Percent of similar responses for "foil" stimuli.}
+#'   \item{p_sim_old}{Percent of similar responses for "target" (i.e., "old") stimuli.}
 #'   \item{bps_score}{BPS score.}
 #'   \item{is_normal}{Checking result whether the data is normal.}
 #' }
@@ -17,21 +21,33 @@ bps <- function(data, ...) {
     warning("`Phase`, `Resp`, `Type`, `RT` and `ACC` variables are required.")
     return(
       data.frame(
+        pc = NA_real_,
+        p_sim_lure = NA_real_,
+        p_sim_foil = NA_real_,
+        p_sim_old = NA_real_,
         bps_score = NA_real_,
         is_normal = FALSE
       )
     )
   }
+  pc_all <- data %>%
+    dplyr::filter(.data$Phase == "test") %>%
+    dplyr::summarise(pc = mean(.data$ACC == 1))
   bps_score <- data %>%
     dplyr::filter(.data$Phase == "test") %>%
     dplyr::group_by(.data$Type) %>%
     dplyr::summarise(p_sim = sum(.data$Resp == "Similar") / dplyr::n()) %>%
     tidyr::pivot_wider(names_from = "Type", values_from = "p_sim") %>%
-    dplyr::transmute(bps_score = .data$lure - .data$foil)
+    dplyr::transmute(
+      p_sim_lure = .data$lure,
+      p_sim_foil = .data$foil,
+      p_sim_old = .data$target,
+      bps_score = .data$lure - .data$foil
+    )
   is_normal <- data %>%
     dplyr::filter(.data$Phase == "test") %>%
     dplyr::mutate(acc_adj = dplyr::if_else(.data$RT >= 100, .data$ACC, 0L)) %>%
     dplyr::summarise(n = dplyr::n(), count_correct = sum(.data$acc_adj == 1)) %>%
     dplyr::transmute(is_normal = .data$n > stats::qbinom(0.95, .data$n, 0.5))
-  cbind(bps_score, is_normal)
+  cbind(pc_all, bps_score, is_normal)
 }
