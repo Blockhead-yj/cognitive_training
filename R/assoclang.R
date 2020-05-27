75f1e017c3c70cc0d60a0ec207ae93d1d14f74e6
Liang Zhang <psychelzh@outlook.com>
2020-05-28 01:15:33 +0800

    Add support for AssocLang game
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ R/assoclang.R
@@ -0,0 +1,35 @@
+#' Calculates index scores for AssocLang game.
+#'
+#' The count of correct responses is returned.
+#'
+#' @param data Raw data of class \code{data.frame}.
+#' @param ... Other input argument for future expansion.
+#' @return A \code{data.frame} contains following values:
+#' \describe{
+#'   \item{count_correct}{Count of correct responses.}
+#'   \item{pc}{Percent of correct responses.}
+#'   \item{is_normal}{Checking result whether the data is normal.}
+#' }
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @export
+assoclang <- function(data, ...) {
+  if (!all(utils::hasName(data, "Correctness"))) {
+    warning("`Correctness` variable is required.")
+    return(
+      data.frame(
+        count_correct = NA_real_,
+        pc = NA_real_,
+        is_normal = FALSE
+      )
+    )
+  }
+  correctness <- gsub("NULL", "", data$Correctness) %>%
+    paste(collapse = "-") %>%
+    strsplit("-") %>%
+    unlist() %>%
+    as.numeric()
+  count_correct <- sum(correctness, na.rm = TRUE)
+  pc <- mean(correctness, na.rm = TRUE)
+  data.frame(count_correct, pc, is_normal = TRUE)
+}
