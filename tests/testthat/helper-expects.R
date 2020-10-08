3707724f84416b5370058a81aa1e2ab5f9204469
Liang Zhang <psychelzh@outlook.com>
2020-10-08 16:11:23 +0800

    🧪test(fix): fix helper errors
    `expect_equivalent` was once used, but it requires the names of the
    resulting `data.frame` the same as what we give,
    which is quite trivial for these tests.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/helper-expects.R
+++ tests/testthat/helper-expects.R
@@ -1,10 +1,11 @@
 #' Test on normal data input
 #'
-#' The normal data from a given file will be imported and then passed into the
-#' function being tested. The result will be compared with that imported from a
-#' given result file based on a specified tolerance. This comparison does take
-#' attributes into consideration.
-test_normal <- function(test_fun, data_file, result_file, ..., tolerance = 1e-3) {
+#' The normal data sample from a given file will be imported and then passed
+#' onto the function being tested. The result will be compared with the expected
+#' result which is imported from a given result file. This comparison does take
+#' attributes into consideration, and the tolerance is also based on the
+#' precision of each value from the file.
+test_normal <- function(test_fun, data_file, result_file, ...) {
   expect_silent(
     result <- test_fun(
       jsonlite::read_json(data_file, simplifyVector = TRUE),
@@ -12,7 +13,19 @@ test_normal <- function(test_fun, data_file, result_file, ..., tolerance = 1e-3)
     )
   )
   expect_result <- jsonlite::read_json(result_file, simplifyVector = TRUE)
-  expect_equivalent(result, expect_result, tolerance = tolerance)
+  expect_named(result, names(expect_result), ignore.order = TRUE)
+  for (index in names(expect_result)) {
+    cur_val <- result[[index]]
+    cur_exp <- expect_result[[index]]
+    # the precision is set as the same as that in the expect result file
+    tol_digit <- 0
+    if (is.double(cur_exp)) {
+      tol_digit <- as.character(cur_exp) %>%
+        stringr::str_extract(r"((?<=\.)\d+)") %>%
+        nchar()
+    }
+    expect_equal(cur_val, cur_exp, tolerance = 10 ^ (- tol_digit))
+  }
 }
 #' Test on abnormal data input
 #'
@@ -22,7 +35,7 @@ test_normal <- function(test_fun, data_file, result_file, ..., tolerance = 1e-3)
 test_abnormal <- function(test_fun, result_file, warn_msg, ...) {
   expect_warning(result <- test_fun(data.frame(), ...), warn_msg)
   expect_result <- jsonlite::read_json(result_file, simplifyVector = TRUE)
-  expect_named(result, names(expect_result))
+  expect_named(result, names(expect_result), ignore.order = TRUE)
   for (index in names(expect_result)) {
     if (index != "is_normal") {
       expect_true(is.na(result[[index]]))
