360cd9629c9972c3c2bc30cbb4396d6ef73e2575
Liang Zhang <psychelzh@outlook.com>
2020-10-08 18:27:56 +0800

    🦄refactor(merge): add return pc value
    This is just a functional combination of `assoclang` and the old `countcorrect`.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/countcorrect.R
+++ R/countcorrect.R
@@ -6,29 +6,45 @@
 #' @param ... Other input argument for future expansion.
 #' @return A `data.frame` contains following values:
 #'   \item{nc}{Count of correct responses.}
+#'   \item{pc}{Percent of correct responses.}
 #'   \item{is_normal}{Checking result whether the data is normal.}
 #' @importFrom rlang .data
-#' @importFrom rlang !!
 #' @export
 countcorrect <- function(data, ...) {
-  acc_vars <- c("ACC", "Repetition")
+  acc_vars <- c("ACC", "Repetition", "Correctness")
   acc_var_idx <- utils::hasName(data, acc_vars)
-  if (!any(acc_var_idx)) {
-    warning("Accuracy related variable, i.e., `ACC` or `Repetition`, is required.")
+  if (sum(acc_var_idx) != 1) {
+    warning("One and only one accuracy related variable, i.e., `ACC`, `Repetition` or `Correctness`, is required.")
     return(
-      data.frame(nc = NA_real_, is_normal = FALSE)
+      data.frame(nc = NA, pc = NA, is_normal = FALSE)
     )
   }
   acc_var <- acc_vars[acc_var_idx]
   if (utils::hasName(data, "RT")) {
     data_adj <- data %>%
       dplyr::mutate(
-        acc_adj = dplyr::if_else(.data$RT >= 100, !!rlang::sym(acc_var), 0L)
+        acc_adj = dplyr::if_else(.data$RT >= 100, .data[[acc_var]], 0L)
       )
   } else {
     data_adj <- data %>%
-      dplyr::mutate(acc_adj = !!rlang::sym(acc_var))
+      dplyr::mutate(acc_adj = .data[[acc_var]])
+  }
+  if (is.character(data_adj$acc_adj)) {
+    data_adj <- data_adj %>%
+      dplyr::filter(!is.na(.data$acc_adj) & .data$acc_adj != "NULL") %>%
+      dplyr::summarise(
+        acc_adj = .data$acc_adj %>%
+          stringr::str_c(collapse = "-") %>%
+          stringr::str_split("-", simplify = TRUE) %>%
+          as.numeric() %>%
+          list()
+      ) %>%
+      tidyr::unnest(.data$acc_adj)
   }
   data_adj %>%
-    dplyr::summarise(nc = sum(.data$acc_adj == 1), is_normal = TRUE)
+    dplyr::summarise(
+      nc = sum(.data$acc_adj == 1),
+      pc = mean(.data$acc_adj == 1),
+      is_normal = TRUE
+    )
 }
