a6ee7463becf77340e2004646072ebd98c343e5f
Liang Zhang <psychelzh@outlook.com>
2020-10-13 15:26:05 +0800

    🐳chore(test): add batch test func
    Regular batch test can be done in this way.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/helper-expects.R
+++ tests/testthat/helper-expects.R
@@ -34,9 +34,9 @@ test_normal <- function(test_fun, data_file, result_file, ...) {
 }
 #' Test on abnormal data input
 #'
-#' The abnormal data input used here is just an empty `data.frame`. The
-#' expected result of call will have all `NA` values but one
-#' `is_normal` variable with value of `FALSE`.
+#' The abnormal data input used here is just an empty `data.frame`. The expected
+#' result of call will have all `NA` values but one `is_normal` variable with
+#' value of `FALSE`.
 test_abnormal <- function(test_fun, result_file, warn_msg, ...) {
   expect_warning(result <- test_fun(data.frame(), ...), warn_msg)
   expect_result <- jsonlite::read_json(result_file, simplifyVector = TRUE)
@@ -48,4 +48,48 @@ test_abnormal <- function(test_fun, result_file, warn_msg, ...) {
       expect_false(result[[index]])
     }
   }
+}
+#' Perform regular batch tests for a function
+#'
+#' Regularly, a test on the sample data files and a test with empty `data.frame`
+#' input will be performed. This function does all this in a batch.
+test_batch <- function(test_fun, ...,
+                       warn_msg = "At least one of the required variables are missing.") {
+  test_fun_str <- as_name(enquo(test_fun))
+  test_that(
+    stringr::str_glue("`{test_fun_str}` should work on all the sample data"), {
+      sample_files <- list.files(
+        file.path("data", test_fun_str),
+        "sample", full.names = TRUE
+      )
+      for (sample_file in sample_files) {
+        sample_label <- stringr::str_extract(sample_file, r"((?<=_)\w+(?=\.))")
+        result_file <- file.path(
+          dirname(sample_file),
+          stringr::str_c("result_", sample_label, ".json")
+        )
+        test_normal(
+          test_fun,
+          data_file = sample_file,
+          result_file = result_file,
+          ...
+        )
+      }
+    }
+  )
+  test_that(
+    stringr::str_glue(
+      "`{test_fun_str}` ",
+      "should output result with 'missing' values if input data is corrupted"
+    ),
+    test_abnormal(
+      test_fun,
+      result_file = list.files(
+        file.path("data", test_fun_str),
+        "result", full.names = TRUE
+      )[[1]],
+      warn_msg = warn_msg,
+      ...
+    )
+  )
 }
